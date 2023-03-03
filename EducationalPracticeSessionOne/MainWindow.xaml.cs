1b35960aec655488d2c1225a40c6840faaf35945
temamironovonline <a.sergeichev03@mail.ru>
2023-03-03 11:17:22 +0300

    Добавьте файлы проекта.

---
+++ EducationalPracticeSessionOne/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace EducationalPracticeSessionOne
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+
+    public partial class MainWindow : Window
+    {
+        public static DBEntities entities;
+        public static Frame frame;
+        public MainWindow()
+        {
+            InitializeComponent();
+            entities = new DBEntities();
+            frame = mainFrame;
+        }
+    }
+}
